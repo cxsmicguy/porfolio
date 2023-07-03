c1d9cecd85241594259f4d4316010349c68ffa43
cxsmicguy <cxsmicguy@gmail.com>
2023-07-03 17:46:36 +0530

    removed my works

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -19,9 +19,6 @@ export default function Footer() {
         <h2 className="mb-6 text-sm font-bold uppercase">Others</h2>
             <ul className="font-medium">
                 <li className="mb-4">
-                    <a href="https://cosmicguy.online" className="hover:underline">My Works</a>
-                </li>
-                <li className="mb-4">
                     <a href="https://www.upwork.com/freelancers/~0190b2baadbcdfc307" className="hover:underline">Upwork</a>
                 </li>
             </ul>
